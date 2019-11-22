4cf73287e56f4cd086705daa409bdb3e42342f05
Vaidas Bagdonas <vaidas.bagdonas@comsave.com>
2019-11-22 16:38:14 +0100

    add maker bundle to dev

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
